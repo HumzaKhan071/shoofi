cf6bd530c3c63f81be855c6e07c0104f47da40cd
Humza Khan <83923770+HumzaKhan071@users.noreply.github.com>
2022-12-02 18:28:07 +0500

    fixes

--- lib/Screens/Widgets/Agreements.dart
+++ lib/Screens/Widgets/Agreements.dart
@@ -17,7 +17,7 @@ class Agreements extends StatelessWidget {
               TextSpan(
                   recognizer: TapGestureRecognizer()
                     ..onTap = (() {
-                      Get.to(() => UserAgreementScreen());
+                      termsAndConditons(context);
                     }),
                   text: " Terms of Service",
                   style: TextStyle(
@@ -40,7 +40,7 @@ class Agreements extends StatelessWidget {
               TextSpan(
                   recognizer: TapGestureRecognizer()
                     ..onTap = () {
-                      Get.to(() => PrivacyScreen());
+                      termsAndConditons(context);
                     },
                   text: " Privacy Policies",
                   style: TextStyle(
@@ -52,5 +52,41 @@ class Agreements extends StatelessWidget {
             ])),
       ],
     );
+  }
+
+  termsAndConditons(context) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return Dialog(
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(10),
+                  topRight: Radius.circular(10),
+                  bottomLeft: Radius.circular(20),
+                  bottomRight: Radius.circular(20),
+                )),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                SizedBox(height: 10),
+                CustomAppBar(title: "Terms and Conditions"),
+                LinearProgressIndicator(
+                  value: 0,
+                  backgroundColor: grey,
+                ),
+                Center(
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                        top: 25.0, bottom: 25, left: 10, right: 10),
+                    child: Text(
+                        textAlign: TextAlign.center,
+                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmo. "),
+                  ),
+                ),
+              ],
+            ),
+          );
+        });
   }
 }
